b9d27037505b5d586212bf299b9fca25102ef564
Hatice Adıgüzel <77553404+haticeadiguzel@users.noreply.github.com>
2021-11-19 22:30:06 +0300

    Add files via upload

---
+++ QT_Gazi_Uzay_UI_2/Designer.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/home/htc/Documents/QT_Projects/untitled/mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(960, 672)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.graphicsALT = qfi_ALT(self.centralwidget)
+        self.graphicsALT.setEnabled(False)
+        self.graphicsALT.setGeometry(QtCore.QRect(640, 20, 300, 300))
+        self.graphicsALT.setMinimumSize(QtCore.QSize(300, 300))
+        self.graphicsALT.setMaximumSize(QtCore.QSize(300, 300))
+        self.graphicsALT.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.graphicsALT.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.graphicsALT.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        self.graphicsALT.setBackgroundBrush(brush)
+        self.graphicsALT.setInteractive(False)
+        self.graphicsALT.setObjectName("graphicsALT")
+        self.graphicsASI = qfi_ASI(self.centralwidget)
+        self.graphicsASI.setEnabled(False)
+        self.graphicsASI.setGeometry(QtCore.QRect(20, 20, 300, 300))
+        self.graphicsASI.setMinimumSize(QtCore.QSize(300, 300))
+        self.graphicsASI.setMaximumSize(QtCore.QSize(300, 300))
+        self.graphicsASI.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.graphicsASI.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.graphicsASI.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        self.graphicsASI.setBackgroundBrush(brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
+        brush.setStyle(QtCore.Qt.NoBrush)
+        self.graphicsASI.setForegroundBrush(brush)
+        self.graphicsASI.setInteractive(False)
+        self.graphicsASI.setObjectName("graphicsASI")
+        self.graphicsHI = qfi_HI(self.centralwidget)
+        self.graphicsHI.setEnabled(False)
+        self.graphicsHI.setGeometry(QtCore.QRect(330, 330, 300, 300))
+        self.graphicsHI.setMinimumSize(QtCore.QSize(300, 300))
+        self.graphicsHI.setMaximumSize(QtCore.QSize(300, 300))
+        self.graphicsHI.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.graphicsHI.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.graphicsHI.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        self.graphicsHI.setBackgroundBrush(brush)
+        self.graphicsHI.setInteractive(False)
+        self.graphicsHI.setObjectName("graphicsHI")
+        self.graphicsAI = qfi_AI(self.centralwidget)
+        self.graphicsAI.setEnabled(False)
+        self.graphicsAI.setGeometry(QtCore.QRect(330, 20, 300, 300))
+        self.graphicsAI.setMinimumSize(QtCore.QSize(300, 300))
+        self.graphicsAI.setMaximumSize(QtCore.QSize(300, 300))
+        self.graphicsAI.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.graphicsAI.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.graphicsAI.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        self.graphicsAI.setBackgroundBrush(brush)
+        self.graphicsAI.setInteractive(False)
+        self.graphicsAI.setObjectName("graphicsAI")
+        self.graphicsTC = qfi_TC(self.centralwidget)
+        self.graphicsTC.setEnabled(False)
+        self.graphicsTC.setGeometry(QtCore.QRect(20, 330, 300, 300))
+        self.graphicsTC.setMinimumSize(QtCore.QSize(300, 300))
+        self.graphicsTC.setMaximumSize(QtCore.QSize(300, 300))
+        self.graphicsTC.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.graphicsTC.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.graphicsTC.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        self.graphicsTC.setBackgroundBrush(brush)
+        self.graphicsTC.setInteractive(False)
+        self.graphicsTC.setObjectName("graphicsTC")
+        self.graphicsVSI = qfi_VSI(self.centralwidget)
+        self.graphicsVSI.setEnabled(False)
+        self.graphicsVSI.setGeometry(QtCore.QRect(640, 330, 300, 300))
+        self.graphicsVSI.setMinimumSize(QtCore.QSize(300, 300))
+        self.graphicsVSI.setMaximumSize(QtCore.QSize(300, 300))
+        self.graphicsVSI.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.graphicsVSI.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.graphicsVSI.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        self.graphicsVSI.setBackgroundBrush(brush)
+        self.graphicsVSI.setInteractive(False)
+        self.graphicsVSI.setObjectName("graphicsVSI")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 960, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Ground Control"))
+
+
+from qfi.qfi_ASI import qfi_ASI
+from qfi.qfi_HI import qfi_HI
+from qfi.qfi_TC import qfi_TC
+from qfi.qfi_VSI import qfi_VSI
+from qfi.qfi_AI import qfi_AI
+from qfi.qfi_ALT import qfi_ALT
